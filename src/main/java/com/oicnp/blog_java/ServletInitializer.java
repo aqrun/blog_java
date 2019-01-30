0129cea17ca03441070439590330ede2c698b9b7
Aqrun <aqrun@sina.com>
2019-01-30 20:03:14 +0800

    init

---
+++ src/main/java/com/oicnp/blog_java/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.oicnp.blog_java;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BlogJavaApplication.class);
+    }
+
+}
+
---
+++ src/test/java/com/oicnp/blog_java/BlogJavaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.oicnp.blog_java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogJavaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
